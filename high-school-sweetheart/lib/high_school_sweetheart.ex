a8909b61fdc9c5d1d1cdd2679f0a4de6bec022a7
cheropChirchir7963 <cheropmarlene@gmail.com>
2022-02-02 14:40:06 +0300

    added high-school-sweetheart

---
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,46 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    String.trim(name)
+    |> String.first()
+  end
+
+  def initial(name) do
+    first_letter(name)
+    |> String.upcase()
+    |> Kernel.<>(".")
+  end
+
+  def initials(full_name) do
+    first_name=
+    String.split(full_name)
+    |> List.first()
+    |> initial()
+    last_name=
+    String.split(full_name)
+    |> List.last()
+    |> initial()
+    first_name <> " " <> last_name
+
+  end
+
+  def pair(full_name1, full_name2) do
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+
+
+  end
+end
